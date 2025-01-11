c4b2ed9211b864d80884bc0d34bdcd2fc748c652
buildstring <70457037+buildstring@users.noreply.github.com>
2025-01-11 15:12:42 -0800

    Update Short Encoding of Words.java

--- Short Encoding of Words.java
+++ Short Encoding of Words.java
@@ -34,7 +34,7 @@ class Solution {
             
             boolean newWord = false;
             for (int i = word.length() - 1; i >= 0; i--) {
-                if (p.isEnd && !newWord) res -= (word.length() - i);
+                if (p.isEnd && !newWord) res -= (word.length() - i); // if we visit a old word which is the prefix of this new word, we need to subtract it from answer
                 if (p.next[word.charAt(i) - 'a'] == null) { // expand what we have in the trie
                     p.isEnd = false;
                     newWord = true;
